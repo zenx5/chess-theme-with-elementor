d5674a030d909dd32b820fb1fa97af9a7fca9004
Octavio Martinez <omartinez@logoscorp.com>
2022-01-20 20:58:55 -0400

    funttions

--- functions.php
+++ functions.php
@@ -1,4 +1,6 @@
 <?php
+include 'classChessTheme.php';
+
 function enqueue_styles_child_theme() {
 
 	$parent_style = 'Hello-Elementor-style';
@@ -14,42 +16,6 @@ function enqueue_styles_child_theme() {
 				);
 }
 
-function shortcode_chess_board($atts){
-	$width = isset( $atts['width'] ) ? $atts['width'].'px' : '500px';
-	ob_start();
-  	?>
-	<style>
-		div.white-1e1d7{
-			background-color: #f0d9b5;
-			color: #b58863;
-		}
-		div.black-3c85d{
-			background-color: red;
-			color: #f0d9b5;
-		}
-  	</style>
-	  <div id="ChessBoard" width="<?=$width;?>"></div>
-  <?php
-  $html = ob_get_contents();
-  ob_clean();
-  return $html;
-}
-
-function chess_board(){
-  $dominio = "https://".$_SERVER['SERVER_NAME'];
-  $pieces = $dominio."/wp-content/themes/chess-theme-with-elementor/resources/img/{piece}.png";
-	?>
-  	<script>
-    	localStorage.setItem('pieceTheme',"<?=$pieces?>");
-  	</script>
-	<link rel="stylesheet" href="<?=$dominio?>/wp-content/themes/chess-theme-with-elementor/resources/css/chessboard-0.3.0.min.css"/>
-	<script src="<?=$dominio?>/wp-content/themes/chess-theme-with-elementor/resources/js/jquery-1.10.1.min.js"></script>
-	<script src="<?=$dominio?>/wp-content/themes/chess-theme-with-elementor/resources/js/chess.min.js"></script>
-	<script src="<?=$dominio?>/wp-content/themes/chess-theme-with-elementor/resources/js/chessboard-0.3.0.min.js" ></script>
-	<script src="<?=$dominio?>/wp-content/themes/chess-theme-with-elementor/resources/js/main.js" ></script>
-	
-	<?php
-}
 
 add_action( 'wp_enqueue_scripts', 'enqueue_styles_child_theme' );
 
